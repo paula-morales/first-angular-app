cee45e3190da1a97c557019907bc7f57ef5ebef9
paula-morales <pmpoclava@gmail.com>
2020-07-26 22:07:57 +0200

    to do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './todo';
+
 @Component({
+  //metadata
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-app';
+  todoValue: string;
+  list: Todo[];
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = '';
+  }
+
+  addItem() {
+    if (this.todoValue) {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false,
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = '';
+  }
+
+  deleteItem(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
+  }
+
+  checkValue(event: any) {
+    console.log(event);
+  }
 }
